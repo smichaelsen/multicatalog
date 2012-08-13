cca5b1a015a79e0ca60403b6b8325de9f43eb2fa
Sebastian Michaelsen <michaelsen@t3seo.de>
2012-08-13 12:20:10 +0200

    Release of 0.7.5
    [TASK] Release of 0.7.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "multicatalog".
 #
-# Auto generated 13-08-2012 10:20
+# Auto generated 13-08-2012 11:47
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Extensible Product Catalog',
 	'category' => 'plugin',
 	'shy' => 0,
-	'version' => '0.7.4',
+	'version' => '0.7.5',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:27:{s:21:"ext_conf_template.txt";s:4:"b91b";s:12:"ext_icon.gif";s:4:"96e1";s:17:"ext_localconf.php";s:4:"3021";s:14:"ext_tables.php";s:4:"710b";s:14:"ext_tables.sql";s:4:"6b4c";s:32:"icon_tx_multicatalog_article.gif";s:4:"475a";s:33:"icon_tx_multicatalog_category.gif";s:4:"475a";s:32:"icon_tx_multicatalog_product.gif";s:4:"475a";s:9:"README.md";s:4:"ebb0";s:41:"Classes/Controller/CategoryController.php";s:4:"d41d";s:40:"Classes/Controller/ProductController.php";s:4:"76b4";s:47:"Classes/Domain/Repository/ProductRepository.php";s:4:"40f8";s:36:"Configuration/FlexForms/flexform.xml";s:4:"8cd5";s:53:"Configuration/FlexForms/flexform_category_records.xml";s:4:"eb7d";s:29:"Configuration/TCA/Article.php";s:4:"740a";s:30:"Configuration/TCA/Category.php";s:4:"ce63";s:29:"Configuration/TCA/Product.php";s:4:"eae9";s:38:"Configuration/TypoScript/constants.txt";s:4:"f985";s:34:"Configuration/TypoScript/setup.txt";s:4:"3cc9";s:24:"Documentation/manual.sxw";s:4:"0970";s:14:"pi1/ce_wiz.gif";s:4:"870e";s:33:"pi1/class.tx_multicatalog_pi1.php";s:4:"82d2";s:41:"pi1/class.tx_multicatalog_pi1_wizicon.php";s:4:"4bb1";s:17:"pi1/locallang.xml";s:4:"fbc8";s:21:"pi1/res/template.html";s:4:"c05a";s:40:"Resources/Private/Language/locallang.xml";s:4:"e30c";s:43:"Resources/Private/Language/locallang_db.xml";s:4:"de92";}',
+	'_md5_values_when_last_written' => 'a:27:{s:21:"ext_conf_template.txt";s:4:"b91b";s:12:"ext_icon.gif";s:4:"96e1";s:17:"ext_localconf.php";s:4:"3021";s:14:"ext_tables.php";s:4:"710b";s:14:"ext_tables.sql";s:4:"6b4c";s:32:"icon_tx_multicatalog_article.gif";s:4:"475a";s:33:"icon_tx_multicatalog_category.gif";s:4:"475a";s:32:"icon_tx_multicatalog_product.gif";s:4:"475a";s:9:"README.md";s:4:"ebb0";s:41:"Classes/Controller/CategoryController.php";s:4:"d41d";s:40:"Classes/Controller/ProductController.php";s:4:"76b4";s:47:"Classes/Domain/Repository/ProductRepository.php";s:4:"40f8";s:36:"Configuration/FlexForms/flexform.xml";s:4:"8cd5";s:53:"Configuration/FlexForms/flexform_category_records.xml";s:4:"eb7d";s:29:"Configuration/TCA/Article.php";s:4:"740a";s:30:"Configuration/TCA/Category.php";s:4:"ce63";s:29:"Configuration/TCA/Product.php";s:4:"eae9";s:38:"Configuration/TypoScript/constants.txt";s:4:"f985";s:34:"Configuration/TypoScript/setup.txt";s:4:"d0ba";s:24:"Documentation/manual.sxw";s:4:"0970";s:14:"pi1/ce_wiz.gif";s:4:"870e";s:33:"pi1/class.tx_multicatalog_pi1.php";s:4:"82d2";s:41:"pi1/class.tx_multicatalog_pi1_wizicon.php";s:4:"4bb1";s:17:"pi1/locallang.xml";s:4:"fbc8";s:21:"pi1/res/template.html";s:4:"c05a";s:40:"Resources/Private/Language/locallang.xml";s:4:"e30c";s:43:"Resources/Private/Language/locallang_db.xml";s:4:"de92";}',
 	'suggests' => array(
 	),
 );
